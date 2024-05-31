853041e859a4aaf84014fa0f2637e99b6fa514f6
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-31 17:13:22 -0500

    first commit

---
+++ src/test/java/com/example/tareitagtics/TareitaGticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tareitagtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareitaGticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
